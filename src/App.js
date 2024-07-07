83329c1413816aecb48194c43e185c10d49b50e8
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2024-07-07 20:12:29 +0530

    unused imports removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
